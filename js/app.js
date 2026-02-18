12590594a61abb2b4076bd60cc5882266a2d200e
FredArgoX <fredy.argo.3@gmail.com>
2026-02-18 15:22:42 -0600

    fullscreen

--- js/app.js
+++ js/app.js
@@ -1,26 +1,17 @@
-const btn = document.getElementById('fullscreenBtn');
-btn.addEventListener('click', () => {
-    const elem = document.documentElement; // Targets the entire page
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen()
-            .then(() => console.log('Fullscreen entered'))
-            .catch(err => console.error('Fullscreen failed:', err));
-    } else {
-        console.warn('Fullscreen API not supported');
-    }
-});
+const enterFullscreen = document.getElementById("enter-fullscreen");
+const exitFullscreen = document.getElementById("exit-fullscreen");
 
-// Optional: Listen for fullscreen changes
-document.addEventListener('fullscreenchange', () => {
-    if (document.fullscreenElement) {
-        console.log('In fullscreen');
-    } else {
-        console.log('Exited fullscreen');
-    }
-});
-
-
-
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+    enterFullscreen.setAttribute("stroke", "transparent");
+    exitFullscreen.setAttribute("stroke", "rgb(231, 223, 212)");
+  } else {
+    document.exitFullscreen();
+    enterFullscreen.setAttribute("stroke", "rgb(231, 223, 212)");
+    exitFullscreen.setAttribute("stroke", "transparent");
+  }
+}
 
 
 
