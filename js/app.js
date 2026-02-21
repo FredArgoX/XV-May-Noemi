09237e6ca1f12f467b9add29528823a364ab460e
FredArgoX <fredy.argo.3@gmail.com>
2026-02-20 20:44:25 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+// //////////////////////////////////////////////////////////////////////////////////////////
+// FullScreen Functionality
 const enterFullscreen = document.getElementById("enter-fullscreen");
 const exitFullscreen = document.getElementById("exit-fullscreen");
-
 function toggleFullscreen() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
@@ -12,21 +13,8 @@ function toggleFullscreen() {
     exitFullscreen.setAttribute("stroke", "transparent");
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// //////////////////////////////////////////////////////////////////////////////////////////
+// Itinerary Aimation
 // Use Intersection Observer to detect when the table enters the viewport
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -40,7 +28,6 @@ const observer = new IntersectionObserver((entries) => {
 }, {
     threshold: 0.1 // Trigger when 10% of the element is visible; adjust as needed
 });
-
 // Target the container
 const container = document.getElementById('itinerario_table_container');
 if (container) {
